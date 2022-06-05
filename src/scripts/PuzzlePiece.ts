a090697bdc12dcf9a2689f015372ea0198de2daf
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-06-05 17:57:25 +0900

    update

--- src/scripts/PuzzlePiece.ts
+++ src/scripts/PuzzlePiece.ts
@@ -5,11 +5,22 @@ export class PuzzlePice {
   sprite!: PIXI.Sprite
 
   constructor(id: number, field: { x: number; y: number }) {
+    const sprite = new PIXI.Sprite(Globals.resources[`puzzle${id}`].texture)
+
     this.sprite = new PIXI.Sprite(Globals.resources[`puzzle${id}`].texture)
     this.sprite.x = field.x
     this.sprite.y = field.y
-
     this.sprite.anchor.set(0.5)
     this.sprite.scale.set(0.5)
+    this.setInteractive()
+  }
+
+  setInteractive() {
+    this.sprite.interactive = true
+    this.sprite.on('mousedown', this.onTouchStart, this)
+  }
+
+  onTouchStart(e: PIXI.InteractionEvent) {
+    console.info(e)
   }
 }
